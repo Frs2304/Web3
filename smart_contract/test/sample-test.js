5edadafd3458850e5faee91dc5a6b37e9d702052
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-19 23:59:03 +0530

    Update sample-test.js

--- smart_contract/test/sample-test.js
+++ smart_contract/test/sample-test.js
@@ -10,7 +10,7 @@ describe("Greeter", function () {
     expect(await greeter.greet()).to.equal("Hello, world!");
   
     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
+  
     // wait until the transaction is mined  
     await setGreetingTx.wait();
   
