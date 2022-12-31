c005788b8dd166dcbf4388840a5c025caacda0d3
Tushar Mishra <tusharmishra2304@gmail.com>
2022-12-31 23:55:13 +0530

    Update deploy.js

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -6,7 +6,7 @@ const main = async () => {
     
   await transactions.deployed()
 
-  console.log('Transactions deployed to:', transactions.address);
+  console.log('Transactions deployed to:', transactions.address)
 }  
 
 const runMain = async () => {
