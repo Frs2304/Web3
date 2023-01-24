16a5fda893a73f249f4d5c885c6d1c5e4d3d299b
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-24 23:57:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import AddTransactionCard from './components/AddTransactionCard';
-import Header from './components/Header'
+import Header from './components/Header';
 import Hero from './components/Hero'
 import Tabuler from './components/Tabuler'
 import {
