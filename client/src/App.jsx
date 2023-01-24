f892d66191ce97eae78bb0b7dfcf1714f4a2fc82
Tushar Mishra <tusharmishra2304@gmail.com>
2023-01-24 23:58:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import AddTransactionCard from './components/AddTransactionCard';
 import Header from './components/Header';
-import Hero from './components/Hero'
+import Hero from './components/Hero';
 import Tabuler from './components/Tabuler'
 import {
   isWallectConnected,  
