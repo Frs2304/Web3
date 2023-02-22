690b22b5714401128a8b789758277f98852c5299
Tushar Mishra <tusharmishra2304@gmail.com>
2023-02-22 23:25:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,8 +25,7 @@ const App = () => {
         <div className="text-center mb-10">
           <button
             onClick={connectWallet}
-            className="text-white bg-blue-500 py-2 px-5 rounded-xl drop-shadow-xl border border-transparent hover:bg-transparent hover:text-blue-500 hover:border hover:border-blue-500 focus:outline-none focus:ring"
-          >
+            className="text-white bg-blue-500 py-2 px-5 rounded-xl drop-shadow-xl border border-transparent hover:bg-transparent hover:text-blue-500 hover:border hover:border-blue-500 focus:outline-none focus:ring">
             Connect Wallet
           </button>
         </div>
