b14bc990aeb97ab7a5c3def81e542756759d2ed4
Tushar Mishra <tusharmishra2304@gmail.com>
2023-05-09 23:59:53 +0530

    Update deploy.js

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -11,10 +11,10 @@ const main = async () => {
 
 const runMain = async () => {
   try {  
-    await main()
-    process.exit(0)
+    await main();
+    process.exit(0);
   } catch (error) {
-    console.error(error)
+    console.error(error);
     process.exit(1);
   }
 }
